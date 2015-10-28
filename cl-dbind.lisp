c5b6d5d73cb00c2ba1f35b51a831e759b8d4eaeb
DruidGreeneyes <josh.carton@gmail.com>
2015-10-28 10:54:39 +0000

    added (with-dbind-vars)

--- cl-dbind.lisp
+++ cl-dbind.lisp
@@ -7,29 +7,28 @@
 (defun interned-p (sym)
   (find-symbol (symbol-name sym)))
 
-(defmacro d-bind (vars list &body body)
+(defmacro dbind (vars list &body body)
   (let ((gensyms ()))
     (labels
         ((process-symbol (sym)
-           (if (atom sym)
-               (case sym
-                 (_ (let ((s (gensym)))
-                      (push s gensyms)
-                      s))
-                 (t sym))
-               (process-list sym)))
-         
-         (process-list (ls)
-           (if (atom ls)
-               (process-symbol ls)
-               (cons (process-symbol (car ls))
-                     (process-list (cdr ls))))))
+           (case sym
+             (_ (let ((s (gensym)))
+                  (push s gensyms)
+                  s))
+             (t sym))
+           (process-list sym)))
+      
+      (process-list (ls)
+                    (if (atom ls)
+                        (process-symbol ls)
+                        (cons (process-symbol (car ls))
+                              (process-list (cdr ls))))))
 
-      (let ((new-vars (process-list vars)))
-        `(destructuring-bind ,new-vars
-             ,list
-           (declare (ignore ,@gensyms))
-           ,@body)))))
+    (let ((new-vars (process-list vars)))
+      `(destructuring-bind ,new-vars
+           ,list
+         (declare (ignore ,@gensyms))
+         ,@body)))))
 
 (defmacro d-binding (def name vars &rest body)
   (let ((list-vars ()))
@@ -48,10 +47,40 @@
                (let ((item (car lis))
                      (nxt (member-if-not #'interned-p (cdr lis))))
                  (if (null nxt)
-                     `(d-bind ,(getf list-vars item) ,item
+                     `(dbind ,(getf list-vars item) ,item
                         ,@body)
-                     `(d-bind ,(getf list-vars item) ,item
+                     `(dbind ,(getf list-vars item) ,item
                         ,(dbind-list-vars nxt))))))
           
           `(,def ,name ,new-vars
              ,(dbind-list-vars (member-if-not #'interned-p new-vars))))))))
+
+(defmacro with-dbind-vars (vars-place body-place statement)
+  (let* ((vars (nth vars-place statement))
+         (body (nthcdr body-place statement))
+         (st (butlast statement (length body)))
+         (list-vars ()))
+    (labels
+        ((gensym-list-var (var)
+           (if (consp var)
+               (let ((s (gensym)))
+                 (push var list-vars)
+                 (push s list-vars)
+                 s)
+               var))
+         
+         (dbind-list-vars (lis)
+           (let ((item (car lis))
+                 (nxt (member-if-not #'interned-p (cdr lis))))
+             (if (null nxt)
+                 `(dbind ,(getf list-vars item) ,item
+                    ,@body)
+                 `(dbind ,(getf list-vars item) ,item
+                    ,(dbind-list-vars nxt)))))
+         
+         (generate-dbinds (lis)
+           `(,(dbind-list-vars (member-if-not #'interned-p lis)))))
+      
+      (let ((new-vars (mapcar #'gensym-list-var vars)))
+        (setf (nth vars-place st) new-vars)
+        `,(nconc st (generate-dbinds new-vars))))))
